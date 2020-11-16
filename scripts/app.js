1d3e00ccc8a60c46790a7faea801070d9f8d0dfb
tstreefkerk <t.s.streefkerk@student.tudelft.nl>
2020-11-16 23:23:32 +0100

    Splits the HTML and Javascript. Copy pastes a `divTemplate` and assigns unique ids. Next step is filtering.

--- scripts/app.js
+++ scripts/app.js
@@ -5,102 +5,88 @@ function main() {
 
 // Reads the JSON file, iterates over and processes the JSON objects.
 function readJsonFile() {
+    var divTemplate = document.getElementById("assignment0").cloneNode(true);
     $.getJSON("data.json", function (json) {
         $.each(json, function (i, element) {
-            var assignmentObj = createAssignmentObj(element);
-            var assignmentHtml = createAssignmentHtml(assignmentObj);
-            setAssignmentDiv(assignmentHtml);
+            if (i != 0) {
+                var clone = document.createElement("div");
+                clone = divTemplate.cloneNode(true);
+                clone.setAttribute("id", "assignment" + i);
+                document.getElementById("assignments").appendChild(clone);
+            }
+            var assignmentObjTemp = createAssignmentObj(element);
+            eval('var assignmentObj' + i + ' = assignmentObjTemp;');
+            insertObjAttributes(eval('assignmentObj' + i), i);
         });
     });
 }
 
+// Adds the assignment object attributes to the div template.
+function insertObjAttributes(assignmentObj, assignmentNum) {
+    var assignmentDiv = document.getElementById("assignment" + assignmentNum);
+    assignmentDiv.querySelector(".logo").src = assignmentObj.logo;
+    setDivByClass(assignmentDiv, assignmentObj, "company", "company");
+
+    setNew(assignmentDiv, assignmentObj);
+    setFeatured(assignmentDiv, assignmentObj);
+
+    setDivByClass(assignmentDiv, assignmentObj, "position", "position");
+    setDivByClass(assignmentDiv, assignmentObj, "framework1", "level");
+    setDivByClass(assignmentDiv, assignmentObj, "framework2", "role");
+
+    setDivByClassList(assignmentDiv, assignmentObj, "framework3", "languages", 0);
+    setDivByClassList(assignmentDiv, assignmentObj, "framework4", "languages", 1);
+    setDivByClassList(assignmentDiv, assignmentObj, "framework5", "languages", 2);
+    setDivByClassList(assignmentDiv, assignmentObj, "framework6", "tools", 0);
+    setDivByClassList(assignmentDiv, assignmentObj, "framework7", "tools", 1);
+
+    setDivByClass(assignmentDiv, assignmentObj, "postedAt", "postedAt");
+    setDivByClass(assignmentDiv, assignmentObj, "contract", "contract");
+    setDivByClass(assignmentDiv, assignmentObj, "location", "location");
+}
+
+// Sets attributes in the div template based on the div class and the object attribute.
+function setDivByClass(assignmentDiv, assignmentObj, divClass, objAttribute) {
+    assignmentDiv.querySelector("." + divClass).innerHTML = assignmentObj[objAttribute];
+}
+
+// Sets the attributes in the div template based on the div class, the object attribute and the array index.
+function setDivByClassList(assignmentDiv, assignmentObj, divClass, objAttribute, i) {
+    assignmentDiv.querySelector("." + divClass).innerHTML = (assignmentObj[objAttribute][i]) ? assignmentObj[objAttribute][i] : "";
+}
+
+// Based on the isNew attribute of the assignmentObj it sets the .new class to "NEW!" or the .new class gets removed.
+function setNew(assignmentDiv, assignmentObj) {
+    if (assignmentObj.isNew) {
+        assignmentDiv.querySelector(".new").innerHTML = "NEW!";
+    } else {
+        assignmentDiv.querySelector(".new").remove();
+    }
+}
+
+// Based on the isNew attribute of the assignmentObj it sets the .featured class to "FEATURED" or the .featured class gets removed.
+function setFeatured(assignmentDiv, assignmentObj) {
+    if (assignmentObj.featured) {
+        assignmentDiv.querySelector(".featured").innerHTML = "FEATURED";
+    } else {
+        assignmentDiv.querySelector(".featured").remove();
+    }
+}
+
 // Creates the Assignment object.
 function createAssignmentObj(element) {
-    return new Assignment(element.id, element.company, element.logo, element.new,element.featured, element.position, 
+    return new Assignment(element.id, element.company, element.logo, element.new, element.featured, element.position,
         element.role, element.level, element.postedAt, element.contract, element.location, element.languages, element.tools);
-}
-
-// Creates the HTML for the Assignment object.
-function createAssignmentHtml(assignment) {
-    var layout = [];
-
-    layout.push(
-        "<div id='assignment'>",
-            "<div class='well'>",
-                "<div class='row'>",
-                    "<div class='col-md-1'>",
-                        "<img src=", assignment.logo, " />",
-                    "</div>",
-                    "<div class='col-md-11'>",
-                        "<ul id='listOuter'>",
-                            "<li>",
-                                "<companyName onclick='temp()'>", assignment.company, "</companyName>",
-                                isNew(assignment.isNew),
-                                isFeatured(assignment.featured),
-                            "</li>",
-                            "<li>",
-                                "<jobDescription>", assignment.position, "</jobDescription>");
-
-                                layout.push(getFrameworks(assignment, false));
-                                layout.push(getFrameworks(assignment, true));
-
-                                layout.push(
-                                "<frameworks onclick='addFilterParameter(this)'>", assignment.level, "</frameworks>",
-                                "<frameworks>", assignment.role, "</frameworks>",
-                            "</li>",
-                            "<li>",
-                                "<infoFirst>", assignment.postedAt, " <span>&#8226;</span> </infoFirst>",
-                                "<info>", assignment.contract, " <span>&#8226;</span> </info>",
-                                "<info>", assignment.location, "</info>",
-                            "</li>",
-                        "</ul>",
-                    "</div>",
-                "</div>",
-            "</div>",
-        "</div>",
-    );
-
-    return layout.join("");
-}
-
-// This was an attempt to implement the filter paramter buttons but I got stuck.
-function addFilterParameter(element) {
-    var text = element.innerHTML;
-    var textObj = document.createElement("filterButton");
-    textObj.innerHTML = text;
-    document.getElementById("row1").appendChild(textObj);
-}
-
-// Creates and sets the Assignment object div.
-function setAssignmentDiv(assignmentHtml) {
-    var assignementDiv = document.createElement("div");
-    assignementDiv.innerHTML = assignmentHtml;
-    document.getElementById("mainLayout").appendChild(assignementDiv);
 }
 
 // Based on the boolean `isNew`, the "NEW!" tag is retrieved.
 function isNew(isNew) {
-    return (isNew) ? "<newTag>NEW!</newTag>" : "";
+    return (isNew) ? "NEW!" : "";
 }
 
 // Based on the boolean `isFeatured`, the "FEATURED" tag is retrieved.
-function isFeatured(isFeatured) {
-    return (isFeatured) ? "<featured>FEATURED</featured>" : "";
-}
-
-// Based on the value of the boolean `isLanguages`, the languages or the tools are iterated and retrieved.
-function getFrameworks(assignment, isLanguages) {
-    var frameworks = (isLanguages) ? assignment.languages : assignment.tools;
-    var frameworksHtml = [];
-
-    var len = frameworks.length;
-    for (j = len - 1; j >= 0; j--) {
-        frameworksHtml.push(
-            "<frameworks>", frameworks[j], "</frameworks>"
-        );
-    }
-
-    return frameworksHtml.join("");
+function isFeatured(featured) {
+    return (featured) ? "FEATURED" : "";
 }
 
 // Class object used to proces the assignements.
@@ -121,4 +107,12 @@ class Assignment {
         this.tools = tools;
     }
 }
+
+$(document).ready(function () {
+    main();
+})
+
+
+
+
 
